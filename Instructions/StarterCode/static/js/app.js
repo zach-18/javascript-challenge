66a7c621de0456395c0991ef855dea85efd0986b
zach-18 <67443071+zach-18@users.noreply.github.com>
2020-12-07 19:19:47 -0600

    Mon Evening Update

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,37 +1,38 @@
+// *** NOTE ***
+// Code taken from Activity 14-3.03-Evr_D3_Table 
+
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// // Console.log the weather data from data.js
-// console.log(data);
+// // // Console.log the weather data from data.js
+console.log(data);
 
+// // // Step 1: Loop Through `data` and console.log each UFO object
+data.forEach(function(UFOdata) {
+  console.log(UFOdata);
+});
 
-// // Step 1: Loop Through `data` and console.log each weather report object
-// data.forEach(function(UFOdata) {
-//   console.log(UFOdata);
-// });
+// // // Step 2:  Use d3 to append one table row `tr` for each UFO object
+// // // Don't worry about adding cells or text yet, just try appending the `tr` elements.
+data.forEach(function(UFOdata) {
+  console.log(UFOdata);
+  var row = tbody.append("tr");
+});
 
-// // Step 2:  Use d3 to append one table row `tr` for each weather report object
-// // Don't worry about adding cells or text yet, just try appending the `tr` elements.
-// data.forEach(function(UFOdata) {
-//   console.log(UFOdata);
-//   var row = tbody.append("tr");
-// });
+// // // Step 3:  Use `Object.entries` to console.log each UFO value
+data.forEach(function(UFOdata) {
+  console.log(UFOdata);
+  var row = tbody.append("tr");
 
-// // Step 3:  Use `Object.entries` to console.log each weather report value
-// data.forEach(function(UFOdata) {
-//   console.log(UFOdata);
-//   var row = tbody.append("tr");
+  Object.entries(UFOdata).forEach(function([key, value]) {
+    console.log(key, value);
+  });
+});
 
-//   Object.entries(UFOdata).forEach(function([key, value]) {
-//     console.log(key, value);
-//   });
-// });
-
-// // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
+// // // Step 4: Use d3 to append 1 cell per UFO value (weekday, date, high, low)
 // data.forEach(function(UFOdata) {
 //   console.log(UFOdata);
 //   var row = tbody.append("tr");
@@ -44,15 +45,15 @@ var tbody = d3.select("tbody");
 //   });
 // });
 
-// // Step 5: Use d3 to update each cell's text with
-// // weather report values (weekday, date, high, low)
+// // // Step 5: Use d3 to update each cell's text with
+// // // weather report values (weekday, date, high, low)
 data.forEach(function(UFOdata) {
-//   console.log(UFOdata);
+  console.log(UFOdata);
   var row = tbody.append("tr");
   Object.entries(UFOdata).forEach(function([key, value]) {
-    // console.log(key, value);
+    console.log(key, value);
     // Append a cell to the row for each value
-    // in the weather report object
+    // in the UFO object
     var cell = row.append("td");
     cell.text(value);
   });
@@ -61,20 +62,9 @@ data.forEach(function(UFOdata) {
 
 //****/ weatherReport = UFOdata *****
 
-// data.forEach(function(tabledata) {
-//     console.log(tabledata);
-//     var row = tbody.append("tr")
-//     row.append("td").text(UFOdata.datetime)
-//     row.append("td").text(UFOdata.city)
-//     row.append("td").text(UFOdata.state)
-//     row.append("td").text(UFOdata.country)
-//     row.append("td").text(UFOdata.shape)
-//     row.append("td").text(UFOdata.durationMinutes)
-//     row.append("td").text(UFOdata.comments)
 
-// });
-
-// from data.js
+// *** NOTE ***
+// Code taken from Activity 14-3.09-Par_Form_Filter 
 
 
 // Select the button
@@ -108,20 +98,3 @@ function runEnter() {
 }
 
 // tbody.text("");
-
-//   var filteredData = data1;
-// YOUR CODE HERE!
-// Get a reference to the table body
-
-
-// data1.forEach(function(filteredData) {
-//     console.log(filteredData);
-//     var row1 = tbody.append("tr");
-//     Object.entries(filteredData).forEach(function([key, value]) {
-//         console.log(key, value);
-//         // Append a cell to the row for each value
-//         // in the weather report object
-//         var cell1 = row1.append("td");
-//         cell1.text(value);
-//     });
-//     });
